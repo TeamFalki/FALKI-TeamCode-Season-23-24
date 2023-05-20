38672bd416677558e3789c1de6e90a2e7e797f67
CodeMarketplace <mail.codemarketplace@gmail.com>
2023-05-20 19:09:26 +0200

    Commit 2

--- src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOpFALKI.java
+++ src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOpFALKI.java
@@ -2,10 +2,11 @@ package org.firstinspires.ftc.teamcode.TeleOp;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name="TeleOpFALKI", group = "FALKI-TeleOp")
 public class TeleOpFALKI extends LinearOpMode {
-
+final ElapsedTime
 
     @Override
     public void runOpMode() {
