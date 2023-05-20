19c0ac66b96a7ec91ea6f5d3f6e5d45fb6013909
CodeMarketplace <mail.codemarketplace@gmail.com>
2023-05-20 18:59:12 +0200

    Initial commit

---
+++ src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOpFALKI.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="TeleOpFALKI", group = "FALKI-TeleOp")
+public class TeleOpFALKI extends LinearOpMode {
+
+
+    @Override
+    public void runOpMode() {
+
+
+
+    }
+}
